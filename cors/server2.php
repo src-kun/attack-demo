66d3fae87976d4b8bca43b16351a6b7e3d58be18
root <root@localhost.localdomain>
2018-01-24 22:06:16 -0500

    增加cors demo

---
+++ cors/server2.php
@@ -0,0 +1,10 @@
+<?php
+	$headers = getallheaders();
+	foreach ($headers as $name => $value) {
+		echo $name.":".$value."\n";
+	}
+	//echo "\nrequest from ".$headers['Origin']."\n";
+	//echo "response from ".$headers['Host']."\n";
+	header('Access-Control-Allow-Origin:*');
+	header('Access-Control-Allow-Credentials: true');
+?>
